94903d6df0afec134e77301d7148662285658196
MartinLaptop <martinbachmor@gmail.com>
2020-06-21 21:59:33 +0200

    Main Menu
    Basic Menu-No Real Funktions

---
+++ 2DBrawler/Assets/Menu/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnClickedSinglePlayer()
+    {
+        Debug.Log("LocalPlayer");
+    }
+    public void OnClickedMultiPlayer()
+    {
+        Debug.Log("Online");
+    }
+    public void OnClickedOptions()
+    {
+        Debug.Log("Options");
+    }
+    public void OnClickedQuit()
+    {
+        Application.Quit(0);
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
